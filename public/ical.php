e5935e0f5b4f982319654dbedc7680a6749c72f3
Arthur Bols <arthur@bols.dev>
2024-02-15 13:03:56 +0100

    Add LICENSE notices

--- public/ical.php
+++ public/ical.php
@@ -1,5 +1,23 @@
 <?php
 
+/*
+ * This file is part of cw-uurrooster-generator.
+ * Copyright (C) 2022 Arthur Bols
+ *
+ * cw-uurrooster-generator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * cw-uurrooster-generator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with cw-uurrooster-generator.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 require __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/../config/config.php';
 
